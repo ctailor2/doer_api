68b6f0e4a46dbaa2b04e9f3d40b2d0b14a690493
Chirag Tailor <ctailor2@gmail.com>
2016-03-09 22:01:53 -0600

    Add todos#create endpoint

--- app/api/todos.rb
+++ app/api/todos.rb
@@ -4,9 +4,19 @@ class Todos < Base
       authenticate!
     end
 
-    desc 'user todos'
+    desc 'todos list'
     get :index do
       present current_user.todos
+    end
+
+    desc 'create todo'
+    params do
+      requires :todo, type: Hash do
+        requires :task, type: String
+      end
+    end
+    post :create do
+      present current_user.todos.create!(declared(params).todo)
     end
   end
 end
