11bb7de16a5ab22009d4e40fdc2a8a32467e6acf
Chirag Tailor <ctailor2@gmail.com>
2016-05-14 22:41:34 -0500

    Expose goal setting updated_at? attribute to enforce restriction of once daily updates

--- app/models/setting/goal.rb
+++ app/models/setting/goal.rb
@@ -1,7 +1,11 @@
 class Setting::Goal < ActiveRecord::Base
   belongs_to :user
 
+  def updated_today?
+    updated_at.in_time_zone(user.timezone).to_date == ActiveSupport::TimeZone[user.timezone].today
+  end
+
   class Entity < Grape::Entity
-    expose :target, :updated_at
+    expose :target, :updated_today?
   end
 end
