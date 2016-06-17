76b287da92bee982e14b555846aa104a979b5afb
Chirag Tailor <ctailor2@gmail.com>
2016-06-16 21:39:20 -0500

    Goal setting annotations

--- app/models/setting/goal.rb
+++ app/models/setting/goal.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: setting_goals
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  target     :integer          default(2), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_setting_goals_on_user_id  (user_id)
+#
+
 class Setting::Goal < ActiveRecord::Base
   belongs_to :user
 
--- spec/models/setting/goal_spec.rb
+++ spec/models/setting/goal_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: setting_goals
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  target     :integer          default(2), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_setting_goals_on_user_id  (user_id)
+#
+
 require 'rails_helper'
 
 RSpec.describe Setting::Goal, type: :model do
